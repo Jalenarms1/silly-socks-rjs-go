65c62ecbdfe181350c793341c5ea5e9479f5a73e
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2024-09-14 14:58:50 -0700

    deploy

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,7 @@
 
-
-export const apiRoot = "http://localhost:5050/api"
+//  sillysocks-goth-production.up.railway.app
+export const apiRoot = "https://sillysocks-goth-production.up.railway.app/api"
+// export const apiRoot = "http://localhost:5050/api"
 
 // export const get = () => {
 
